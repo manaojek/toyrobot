1f9ac90207b5b22fd3dc4514c8c6ffd4785002df
Komsant Tivong <komsant.tivong@clbs.co.th>
2024-11-23 16:56:10 +0700

    First time commit
    Initial commit

---
+++ ToyRobot/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+namespace ToyRobotChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's go my robot.");
+            Console.WriteLine("Please using below command to control the robot.");
+            Console.WriteLine("PLACE X,Y,F");
+            Console.WriteLine("MOVE");
+            Console.WriteLine("LEFT");
+            Console.WriteLine("RIGHT");
+            Console.WriteLine("REPORT");
+            Console.WriteLine("CTRL C to stop the program.");
+            ToyRobot robot = new ToyRobot();
+            while (true)
+            {
+                String robotCommands = Console.ReadLine();
+                if (robotCommands.Contains("PLACE"))
+                {
+                    string[] commands = robotCommands.Split(" ");
+                    string[] commandParameters = commands[1].Split(",");
+                    Direction directionParameter = (Direction)Enum.Parse(typeof(Direction), commandParameters[2]);
+                    robot.Place(int.Parse(commandParameters[0]),
+                         int.Parse(commandParameters[1]),
+                        directionParameter
+                         );
+                } else if (robotCommands.Contains("MOVE"))
+                {
+                    robot.Move();
+                } else if(robotCommands.Contains("LEFT"))
+                {
+                    robot.Left();
+                } else if(robotCommands.Contains("RIGHT"))
+                {
+                    robot.Right();
+                } else if(robotCommands.Contains("REPORT"))
+                {
+                    Console.WriteLine(robot.Report());
+                }
+
+            }
+        }
+    }
+}
---
+++ ToyRobotChallengeTest/ToyRobotTests.cs
@@ -0,0 +1,89 @@
+﻿using NUnit.Framework;
+using ToyRobotChallenge;
+
+namespace ToyRobotChallengeTest
+{
+    [TestFixture]
+    public class ToyRobotTests
+    {
+        private ToyRobot robot;
+
+        [SetUp]
+        public void Setup()
+        {
+            robot = new ToyRobot();
+        }
+
+        [Test]
+        public void TestInitialReport()
+        {
+            Assert.AreEqual("Robot is not yet placed.", robot.Report());
+        }
+
+        [Test]
+        public void TestValidPlaceAndReport()
+        {
+            robot.Place(2, 3, Direction.NORTH);
+            Assert.AreEqual("2,3,NORTH", robot.Report());
+        }
+
+        [Test]
+        public void TestInvalidPlace()
+        {
+            robot.Place(-1, 0, Direction.EAST);
+            Assert.AreEqual("Robot is not yet placed.", robot.Report());
+        }
+
+        [Test]
+        public void TestMoveWithinBounds()
+        {
+            robot.Place(0, 0, Direction.NORTH);
+            robot.Move();
+            Assert.AreEqual("0,1,NORTH", robot.Report());
+        }
+
+        [Test]
+        public void TestMoveOutOfBounds()
+        {
+            robot.Place(0, 4, Direction.NORTH);
+            robot.Move();
+            Assert.AreEqual("0,4,NORTH", robot.Report());
+        }
+
+        [Test]
+        public void TestLeftRotation()
+        {
+            robot.Place(1, 1, Direction.NORTH);
+            robot.Left();
+            Assert.AreEqual("1,1,WEST", robot.Report());
+        }
+
+        [Test]
+        public void TestRightRotation()
+        {
+            robot.Place(1, 1, Direction.NORTH);
+            robot.Right();
+            Assert.AreEqual("1,1,EAST", robot.Report());
+        }
+
+        [Test]
+        public void TestSequentialCommands()
+        {
+            robot.Place(1, 1, Direction.NORTH);
+            robot.Move();
+            robot.Right();
+            robot.Move();
+            Assert.AreEqual("2,2,EAST", robot.Report());
+        }
+
+        [Test]
+        public void TestWrongSequentialCommands()
+        {
+            robot.Move();
+            robot.Place(1, 1, Direction.NORTH);
+            robot.Right();
+            robot.Move();
+            Assert.AreEqual("2,1,EAST", robot.Report());
+        }
+    }
+}
